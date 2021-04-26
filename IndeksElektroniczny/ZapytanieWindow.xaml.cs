5be70b63bc7d9c6c195151b26218d102ac093ff1
patrykwieczorek03 <patrykwieczorek03@gmail.com>
2021-04-26 22:22:04 +0200

    Okno question

---
+++ IndeksElektroniczny/ZapytanieWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IndeksElektroniczny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ZapytanieWindow.xaml
+    /// </summary>
+    public partial class ZapytanieWindow : Window
+    {
+        public ZapytanieWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
