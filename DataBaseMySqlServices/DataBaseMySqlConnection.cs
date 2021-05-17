07ec952810f5293f6ed61757c4a2857474f7ec8a
patrykwieczorek03 <patrykwieczorek03@gmail.com>
2021-05-17 20:24:50 +0200

    Dodano connection pooling

--- DataBaseMySqlServices/DataBaseMySqlConnection.cs
+++ DataBaseMySqlServices/DataBaseMySqlConnection.cs
@@ -13,7 +13,7 @@ namespace DataBaseMySqlServices
         public MySqlConnection conection;
         public DataBaseMySqlConnection()
         {
-            this.connetionString = @"server=localhost;Uid=IndeksElektroniczny;database=elektroniczny_indeks;Pwd=IndeksElektroniczny;";
+            this.connetionString = @"server=localhost;Uid=IndeksElektroniczny;database=elektroniczny_indeks;Pwd=IndeksElektroniczny;Pooling=True;";
             this.conection = new MySqlConnection(this.connetionString);
             this.conection.Open();
         }
